e749044a325b35a3fc2718385b0fd4a96d3124ac
Ayush Sonawale <sonawaleayush@gmail.com>
2023-03-04 21:21:49 +0530

    Create 509. Fibonacci Number.cpp
    This question solve through recursion method..

---
+++ 509. Fibonacci Number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int fib(int n) {
+        if(n == 0)
+            return 0;
+
+        if(n == 1)
+            return 1;
+            
+        return fib(n-1)+fib(n-2);
+          
+    }
+};
+int main(){
+    Solution s1; // object creation
+
+    int n;
+    cin>>n;
+
+    s1.fib(); // function call
+}
